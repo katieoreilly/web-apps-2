38642b9fe0c9c32643619d261532b20fd112176f
Katie O'Reilly <katie.oreilly@kellogg.northwestern.edu>
2024-02-13 17:30:37 +0000

    class on feb 13

--- app/controllers/bitcoins_controller.rb
+++ app/controllers/bitcoins_controller.rb
@@ -1,8 +1,19 @@
 class BitcoinsController < ApplicationController
 
   def index
-    # add backend code
-    # render bitcoins/index view
-  end
+    bitcoin = params["amount"] 
 
-end
+    # bitcoin = 1.0
+
+    require "net/http"
+    require "json"
+    url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    bitcoin_data = JSON.parse(response)
+
+    @usd_rate = bitcoin_data["bpi"]["USD"]["rate_float"]
+
+    @usd_value = @usd_rate * bitcoin.to_f
+  end
+end   
